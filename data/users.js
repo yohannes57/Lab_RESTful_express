f277a3ff426832650180a3411e1a9e0d67f07801
yohannes57 <ytmderes@gmail.com>
2024-06-11 07:08:31 -0700

    *

---
+++ data/users.js
@@ -0,0 +1,36 @@
+// Creating the simple data structures we'll work with.
+// How we choose to store and organize this data is very important!
+// Different options and techniques for storing data and
+// creating relationships between different data sets will be
+// explored during lessons on database integrations and techniques.
+
+const { CLIENT_RENEG_LIMIT } = require("tls");
+
+// The "users" data will be simple information about
+// the application's user base.
+const users = [
+  {
+    id: 1,
+    name: "Carey",
+    username: "cyare23",
+    email: "cy23@example.com",
+  },
+  {
+    id: 2,
+    name: "Mikoto",
+    username: "Miiko",
+    email: "mikoto_u@example.com",
+  },
+  {
+    id: 3,
+    name: "Ronald",
+    username: "RonRonRon",
+    email: "mronald@example.com",
+  },
+];
+// const findI=users.find((user)=>{
+//     return user.name=="Mikoto";
+// })
+// console.log(findI)
+
+module.exports = users;
---
+++ utilities/error.js
@@ -0,0 +1,6 @@
+function error(status, msg) {
+  var err = new Error(msg);
+  err.status = status;
+  return err;
+}
+module.exports = error;
